d83aefbfa1f095c75531d399ca12bdb5fbfe6b03
Ebube <onyirimbaebubechi1234@gmail.com>
2024-03-04 11:50:12 +0100

    The variable project solution

---
+++ Variable_Project/Program.cs
@@ -0,0 +1,20 @@
+﻿//Develop a basic c# console application that computes and shows the grade entered by the user and it should be in lower case and should not pass the range of f grade.
+// Show the integer value of the grade.
+Console.Write("Enter the grade:");
+char gradeScore = char.Parse(Console.ReadLine());
+int gradeNum;
+if(gradeScore <= 'f')
+{
+    gradeNum = gradeScore;
+    Console.WriteLine("Valid grade {0}", gradeNum);
+}
+else
+{
+    Console.WriteLine("Invalid grade");
+}
+//Develop a basic C# console application that computes and shows the CGPA entered by the user.
+Console.Write("Enter the CGPA: ");
+decimal cgPa = decimal.Parse(Console.ReadLine());
+// I used a decimal datatype over the double datatype because i wanted the cgpa to be more precise.
+
+
