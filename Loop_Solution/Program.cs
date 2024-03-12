b0ef3e31c8441721580bbd5a5ddacf40c5785690
Ebube <onyirimbaebubechi1234@gmail.com>
2024-03-12 09:21:26 +0100

    The Loop project Solution

---
+++ Loop_Solution/Program.cs
@@ -0,0 +1,25 @@
+﻿        Console.WriteLine("Convert Score to Letter Grade"); 
+        Console.Write("Enter the numerical score: ");
+        int score = int.Parse(Console.ReadLine());
+        char letterGrade;
+        if (score >= 90)
+        {
+            letterGrade = 'A';
+        }
+        else if (score >= 80)
+        {
+            letterGrade = 'B';
+        }
+        else if (score >= 70)
+        {
+            letterGrade = 'C';
+        }
+        else if (score >= 60)
+        {
+            letterGrade = 'D';
+        }
+        else
+        {
+            letterGrade = 'F';
+        }
+        Console.WriteLine($"The letter grade for the score {score} is: {letterGrade}");
