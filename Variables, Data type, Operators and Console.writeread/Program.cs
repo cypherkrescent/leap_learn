6827196e9dbe187f81fc61233f011034d53e2cd1
Ebube <onyirimbaebubechi1234@gmail.com>
2024-03-18 16:17:40 +0100

    Solved question 1

---
+++ Variables, Data type, Operators and Console.writeread/Program.cs
@@ -0,0 +1,16 @@
+﻿//Develop a basic c# console application that computes and shows the grade entered by the user and it should be in lower case and should not pass the range of f grade.
+// Show the integer value of the grade.
+
+Console.WriteLine("Enter the grade:");
+char gradeScore = char.Parse(Console.ReadLine());
+if (gradeScore <= 'f')
+{
+    Console.WriteLine("Valid grade {0}", gradeScore);
+}
+else
+{
+    Console.WriteLine("Invalid grade");
+}
+
+
+
